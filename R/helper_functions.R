b1f6f33b5a5ed1848d7fe9548fa7a19bf067ccf2
rjake <rjake@sas.upenn.edu>
2020-06-29 03:30:55 -0400

    Allow spaces or not in regex

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -155,7 +155,7 @@ find_input_code <- function(file){
   } else {# if an Rmd, convert eval = F statement to T to see if "input <-" exists
     replace_evals <-
       read_file(file) %>%
-      str_replace_all("eval = F(ALSE)?", "eval = TRUE")
+      str_replace_all("eval( )?=( )?F(ALSE)?", "eval = TRUE")
     
     output = tempfile()
     # create R doc from Rmd
