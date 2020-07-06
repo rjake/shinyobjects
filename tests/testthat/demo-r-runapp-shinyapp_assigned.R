e8c067ad2ed8cd7c8a97e57e05cf34ffbd84ef0f
rjake <rjake@sas.upenn.edu>
2020-07-05 21:48:39 -0400

    Add lines of code to demo scripts

--- tests/testthat/demo-r-runapp-shinyapp_assigned.R
+++ tests/testthat/demo-r-runapp-shinyapp_assigned.R
@@ -1,5 +1,7 @@
+library(shiny)
+
 # Apps can be run without a server.r and ui.r file
-app <- shinyApp(
+app = shinyApp(
   ui = fluidPage(
     numericInput("x", "# of obs.", 20),
     plotOutput("plot")
@@ -8,6 +10,8 @@ app <- shinyApp(
     my_df <- reactive({
       head(cars, input$x)
     })
+    
+    about_df <- reactiveValues(n_obs = nrow(my_df()), len = length(my_df()))
     
     output$plot <- renderPlot(
       plot(my_df())
