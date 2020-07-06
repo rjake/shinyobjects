9bf5d51e443aafad9708a87e391795c59daf3bd9
rjake <rjake@sas.upenn.edu>
2020-07-05 21:49:42 -0400

    Collapse strings_to_find() and valid_object_names()

--- R/utilities-constants.R
+++ R/utilities-constants.R
@@ -1,11 +1,9 @@
 #' Valid strings for assignments/column names
 #' @noRd
-valid_object_names <- function() {
-  "[\\w\\.\\$0:9]+"
-}
-
-#' Valid strings for assignments/column names
-#' @noRd
 strings_to_find <- function() {
-  paste0("^(library|", valid_object_names(), " (<-|=[^=]))")
+  paste0(
+    "^(library|", 
+    "[\\w\\.\\$0:9]+", 
+    " (<-|=[^=]))"
+  )
 }
